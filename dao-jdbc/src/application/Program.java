7b757044773b0df23f5ec789049b90c07fc7d35a
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-10-12 18:37:05 -0300

    Seller entities class

--- dao-jdbc/src/application/Program.java
+++ dao-jdbc/src/application/Program.java
@@ -1,13 +1,25 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		//SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1,
+				"Rodrigo",
+				"rodrigo@gmail.com",
+				new Date(),
+				3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
