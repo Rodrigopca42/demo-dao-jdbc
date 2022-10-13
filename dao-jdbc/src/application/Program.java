607b3c8bd4410a1d231cf30847a23f46d6251125
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-10-13 10:12:11 -0300

    FindAll Implementation

--- dao-jdbc/src/application/Program.java
+++ dao-jdbc/src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Teste3: seller findByAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj: list) {
+			System.out.println(obj);
+		}
 	}
 
 }
